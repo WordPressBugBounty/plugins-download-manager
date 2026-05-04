2168fecc8619c7b9c4c7bddc48a5e351dd224bb6
Sean Murphy <sean@iamseanmurphy.com>
2026-05-04 01:10:31 +0000

    3.3.55

--- download-manager/download-manager.php
+++ download-manager/download-manager.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.wpdownloadmanager.com/purchases/
 Description: Manage, Protect and Track file downloads, and sell digital products from your WordPress site. A complete digital asset management solution.
 Author: W3 Eden, Inc.
 Author URI: https://www.wpdownloadmanager.com/
-Version: 3.3.54
+Version: 3.3.55
 Text Domain: download-manager
 Domain Path: /languages
 */
